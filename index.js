b56f5ffea6887a564fee5d662a680789b3600b11
Sumit Sasidharan <sumit.onorall@gmail.com>
2022-05-10 12:07:42 +0530

    updated index.js server

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const webSocketsServerPort = 8000
-const webSocketServer = require('websocket').server
-const http = require('http')
+const webSocketsServerPort = 8000;
+const webSocketServer = require("websocket").server;
+const http = require("http");
 
 // Spinning the http server and the websocket sever.
 const server = http.createServer();
@@ -8,17 +8,47 @@ server.listen(webSocketsServerPort);
 console.log("listening on port 8000");
 
 const wsServer = new webSocketServer({
-    httpServer: server
-})
+  httpServer: server,
+});
 
-const clients = {}
+const clients = {};
 
 // this code generates unique userid for everyuser
 const getUniqueID = () => {
-    const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
-    return s4() + s4() + '-' + s4();
-}
+  const s4 = () =>
+    Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  return s4() + s4() + "-" + s4();
+};
 
-wsServer.on('request', (req) => {
-    const userID = getUniqueID();
-})
+wsServer.on("request", (request) => {
+  const userID = getUniqueID();
+  console.log(
+    new Date() +
+      " Received a new connection from origin " +
+      request.origin +
+      "."
+  );
+
+  // You can rewrite this part of the code to accept only the requests from allowed origin
+  const connection = request.accept(null, request.origin);
+  clients[userID] = connection;
+  console.log(
+    "connected: " + userID + " in " + Object.getOwnPropertyNames(clients)
+  );
+
+  connection.on("message", (message) => {
+    if (message.type === "utf8") {
+      console.log("Received Message: ", message.utf8Data);
+
+      // broadcasting message to all connected clients
+      for (key in clients) {
+        clients[key].sendUTF(message.utf8Data);
+        console.log("sent Message to: ", clients[key]);
+      }
+    }
+  });
+});
+
+// 6.07
